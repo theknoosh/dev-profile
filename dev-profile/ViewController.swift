40020691619c142787d545bf593c0c7a5b8c604e
Darrell Payne <d.anthony.payne@gmail.com>
2017-12-05 12:05:55 -0800

    Layout started (no code), not going well

--- dev-profile/ViewController.swift
+++ dev-profile/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LogoGraphic: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        LogoGraphic.layer.cornerRadius = LogoGraphic.frame.height / 4.0
+        LogoGraphic.layer.masksToBounds = true;
+ 
     }
 
     override func didReceiveMemoryWarning() {
